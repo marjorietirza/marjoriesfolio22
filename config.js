8204f6d7a7db48d98ab9590c8051126547b7b481
Marjorie Tirza Eunike <58949442+marjorietirza@users.noreply.github.com>
2022-06-14 23:43:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
     "Come and take a look",
   KEYWORDS: "Marjorie, Eunike, Product Designer, UX, Portfolio",
   HERO_TITLE:
-    "Hi! I'm Marjorie, a Product Designer based in Jakarta, Indonesia.",
+    "Hi! I'm Marjorie, a Product Designer based in Jakarta, Indonesia 🇮🇩",
   HERO_SUBTITLE: "Currently creating impacts in Indonesia’s growing investment startup, Pluang. Previously at Cashbac — marketing platform startup and Jagoan Hosting Indonesia — one of the top hosting provider in Indonesia.",
   COPYRIGHT: "Marjorie",
   LOCALE: "en",
